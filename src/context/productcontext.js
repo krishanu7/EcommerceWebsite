0cd2d2e00d9b6b142f6008f51cd4d7cb294b9058
Krishanu Saha <93394632+krishanu7@users.noreply.github.com>
2023-05-23 22:18:29 +0530

    Updated

---
+++ src/context/productcontext.js
@@ -0,0 +1,12 @@
+import { createContext, useContext } from "react";
+
+const AppContext = createContext();
+
+const AppProvider = ({children}) => {
+    return <AppContext.Provider value="Krishanu" >{children}</AppContext.Provider>
+};
+const useProductContext = () => {
+    return useContext(AppContext);
+}
+
+export {AppProvider, AppContext, useProductContext};
